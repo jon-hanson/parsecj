783a307501c245cdc136b3ec4f53c2342bb12486
jon hanson <jon-hanson@users.noreply.github.com>
2017-01-03 09:08:42 +0000

    Update IListTest.java

--- src/test/java/org/javafp/data/IListTest.java
+++ src/test/java/org/javafp/data/IListTest.java
@@ -1,57 +1,94 @@
 package org.javafp.data;
 
-import org.junit.*;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class IListTest {
 
-    final IList<Integer> empty = IList.of();
-    final IList<Integer> l = IList.of(1, 2, 3, 4);
+    private static final IList<Integer> empty = IList.of();
+    private static final IList<Integer> l = IList.of(1, 2, 3, 4);
 
     @Test
     public void testEquals() {
-        Assert.assertEquals("equals for an empty list", IList.of(), empty);
-        Assert.assertEquals("[equals for a non-empty list", IList.of().add(4).add(3).add(2).add(1), l);
+        assertEquals("equals for an empty list", IList.of(), empty);
+        assertEquals("[equals for a non-empty list", IList.of().add(4).add(3).add(2).add(1), l);
+
+        final IList<Integer> l3 = IList.of(1, 2, 3);
+        final IList<Integer> l5 = IList.of(1, 2, 3, 4, 5);
+        assertFalse("equals for an different length lists", l3.equals(l));
+        assertFalse("equals for an different length lists", l5.equals(l));
     }
 
     @Test
     public void testIsEmpty() {
-        Assert.assertTrue("isEmpty for an empty list", empty.isEmpty());
-        Assert.assertFalse("isEmpty() for a non-empty list", l.isEmpty());
+        assertTrue("isEmpty for an empty list", empty.isEmpty());
+        assertFalse("isEmpty() for a non-empty list", l.isEmpty());
     }
 
     @Test
     public void testLength() {
-        Assert.assertEquals("size for an empty list", 0, empty.size());
-        Assert.assertEquals("[1,2,3,4].size for a non-empty list", 4, l.size());
+        assertEquals("size for an empty list", 0, empty.size());
+        assertEquals("[1,2,3,4].size for a non-empty list", 4, l.size());
     }
 
     @Test
     public void testToString() {
-        Assert.assertEquals("toString for an empty list", "[]", empty.toString());
-        Assert.assertEquals("toString for a non-empty list", "[1,2,3,4]", l.toString());
+        assertEquals("toString for an empty list", "[]", empty.toString());
+        assertEquals("toString for a non-empty list", "[1,2,3,4]", l.toString());
     }
 
     @Test
     public void testFoldl() {
-        Assert.assertEquals("foldl for an empty list", 0, empty.foldl((x, y) -> x - y, 0).intValue());
-        Assert.assertEquals("foldl for a non-empty list", (((10-1)-2)-3)-4, l.foldl((x, y) -> x - y, 10).intValue());
+        assertEquals("foldl for an empty list", 0, empty.foldl((x, y) -> x - y, 0).intValue());
+        assertEquals("foldl for a non-empty list", (((10-1)-2)-3)-4, l.foldl((x, y) -> x - y, 10).intValue());
     }
 
     @Test
     public void testFoldr() {
-        Assert.assertEquals("foldr for an empty list", 0, empty.foldr((x, y) -> x - y, 0).intValue());
-        Assert.assertEquals("foldr for a non-empty list", 1-(2-(3-(4-10))), l.foldr((x, y) -> x - y, 10).intValue());
+        assertEquals("foldr for an empty list", 0, empty.foldr((x, y) -> x - y, 0).intValue());
+        assertEquals("foldr for a non-empty list", 1-(2-(3-(4-10))), l.foldr((x, y) -> x - y, 10).intValue());
     }
 
     @Test
     public void testMap() {
-        Assert.assertEquals("map for an empty list", IList.of(), empty.map(x -> null));
-        Assert.assertEquals("map for a non-empty list", IList.of(-1, -2, -3, -4), l.map(i -> -i));
+        assertEquals("map for an empty list", IList.of(), empty.map(x -> null));
+        assertEquals("map for a non-empty list", IList.of(-1, -2, -3, -4), l.map(i -> -i));
     }
 
     @Test
     public void testMatch() {
-        Assert.assertTrue("match for an empty list", empty.match(n -> false, e -> true));
-        Assert.assertTrue("match for a non-empty list", l.match(n -> true, e -> false));
+        assertTrue("match for an empty list", empty.match(n -> false, e -> true));
+        assertTrue("match for a non-empty list", l.match(n -> true, e -> false));
+    }
+
+    @Test
+    public void testToList() {
+        final IList<Integer> il = IList.of(0, 1, 2, 3, 4, 5);
+        final List<Integer> l = il.toList();
+        int i = 0;
+        for (int el : l) {
+            assertEquals("", i++, el);
+        }
+    }
+
+    @Test
+    public void testGet() {
+        final IList<Integer> l = IList.of(0, 1, 2, 3);
+        assertEquals("get for a non-empty list", 0, l.get(0).intValue());
+        assertEquals("get for a non-empty list", 1, l.get(1).intValue());
+        assertEquals("get for a non-empty list", 3, l.get(3).intValue());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGet2() {
+        IList.of(0, 1, 2, 3).get(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGet3() {
+        IList.of(0, 1, 2, 3).get(4);
     }
 }
